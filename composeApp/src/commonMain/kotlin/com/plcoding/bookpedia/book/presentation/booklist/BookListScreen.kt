b55f8ae0049fc2e71db414a57300049feea0a394
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-19 19:58:08 +0200

    create listener for view model action;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListScreen.kt
@@ -1,20 +1,37 @@
 package com.plcoding.bookpedia.book.presentation.booklist
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import com.plcoding.bookpedia.book.domain.Book
+import kotlinx.coroutines.flow.distinctUntilChanged
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun BookListScreen(
     modifier: Modifier = Modifier,
     viewModel: BookListViewModel = koinViewModel(),
-    onBookClick: (Book) -> Unit
+    navigateToBookDetails: (Book) -> Unit
 ) {
-
+    val lifecycleOwner = LocalLifecycleOwner.current
     val state by viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(state = Lifecycle.State.STARTED) {
+            viewModel.action.distinctUntilChanged().collect { action ->
+                when (action) {
+                    is BookListViewModelAction.OnNavigateToBookDetails -> {
+                        navigateToBookDetails(action.book)
+                    }
+                }
+            }
+        }
+    }
 
     BookListScreen(
         modifier = modifier,
@@ -29,7 +46,7 @@ fun BookListScreen(
 fun BookListScreen(
     modifier: Modifier,
     state: BookListState,
-    onAction: (BookListAction) -> Unit,
+    onAction: (BookListScreenAction) -> Unit,
 ) {
 
 }
