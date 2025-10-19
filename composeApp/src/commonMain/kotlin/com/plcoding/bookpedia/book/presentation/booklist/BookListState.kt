7d2172a730781c671305acdd98d771979e14a632
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-19 19:24:57 +0200

    update constructor contract (this state will be mutable);

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListState.kt
@@ -3,8 +3,7 @@ package com.plcoding.bookpedia.book.presentation.booklist
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.presentation.UiText
 
-@ConsistentCopyVisibility
-data class BookListState private constructor(
+data class BookListState(
     val searchQuery: String = "",
     val searchResult: List<Book> = emptyList(),
     val favoriteBooks: List<Book> = emptyList(),
