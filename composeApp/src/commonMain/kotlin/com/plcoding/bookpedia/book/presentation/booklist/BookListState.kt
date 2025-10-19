6ce4b41552f4510a9fbf317e49b35777625b7643
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-19 19:01:47 +0200

    change contract of constructor; create factory method;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListState.kt
@@ -3,11 +3,16 @@ package com.plcoding.bookpedia.book.presentation.booklist
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.presentation.UiText
 
-data class BookListState(
+@ConsistentCopyVisibility
+data class BookListState private constructor(
     val searchQuery: String = "",
     val searchResult: List<Book> = emptyList(),
     val favoriteBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null
-)
+) {
+    companion object {
+        fun createDefaultState() = BookListState()
+    }
+}
