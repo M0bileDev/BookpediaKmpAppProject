8cee510c8116238060a60e24c2cca769af713b0b
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-19 19:57:31 +0200

    rename class;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListViewModelAction.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListViewModelAction.kt
@@ -3,5 +3,5 @@ package com.plcoding.bookpedia.book.presentation.booklist
 import com.plcoding.bookpedia.book.domain.Book
 
 sealed interface BookListViewModelAction {
-    data class OpenBookDetails(val book: Book) : BookListViewModelAction
+    data class OnNavigateToBookDetails(val book: Book) : BookListViewModelAction
 }
