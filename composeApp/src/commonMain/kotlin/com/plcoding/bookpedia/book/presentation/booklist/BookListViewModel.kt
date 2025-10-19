e715462420b8b3f100576362b070edb7b0535995
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-19 19:56:29 +0200

    update view model logic;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListViewModel.kt
@@ -1,27 +1,42 @@
 package com.plcoding.bookpedia.book.presentation.booklist
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class BookListViewModel : ViewModel() {
 
     private val _state = MutableStateFlow(BookListState.createDefaultState())
-    val state = _state.asStateFlow()
+    val state get() = _state.asStateFlow()
 
-    fun onAction(action: BookListAction) = when (action) {
-        is BookListAction.OnBookClick -> {}
-        is BookListAction.OnSearchQueryChanged -> {
+    private val _action = MutableSharedFlow<BookListViewModelAction>()
+    val action get() = _action.asSharedFlow()
+
+    fun onAction(action: BookListScreenAction) = when (action) {
+        is BookListScreenAction.OnBookClick -> {
+            viewModelScope.launch {
+                ensureActive()
+                _action.emit(BookListViewModelAction.OpenBookDetails(action.book))
+            }
+        }
+
+        is BookListScreenAction.OnSearchQueryChanged -> {
             _state.update {
                 it.copy(searchQuery = action.query)
             }
         }
 
-        is BookListAction.OnTabSelected -> {
+        is BookListScreenAction.OnTabSelected -> {
             _state.update {
                 it.copy(selectedTabIndex = action.index)
             }
         }
     }
 }
+
