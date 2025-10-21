5299d5fead1d01d193ef274b49f720337a7f7582
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-21 23:34:42 +0200

    add lead icon;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/SearchBar.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/SearchBar.kt
@@ -1,6 +1,7 @@
 package com.plcoding.bookpedia.book.presentation.booklist.components
 
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -8,9 +9,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.search_24px
 import cmp_bookpedia.composeapp.generated.resources.search_hint
+import cmp_bookpedia.composeapp.generated.resources.search_leading_icon
 import com.plcoding.bookpedia.core.presentation.DarkBlue
 import com.plcoding.bookpedia.core.presentation.SandYellow
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -31,6 +35,13 @@ fun SearchBar(
         ),
         placeholder = {
             Text(text = stringResource(Res.string.search_hint))
+        },
+        leadingIcon = {
+            Icon(
+                painter = painterResource(Res.drawable.search_24px),
+                contentDescription = stringResource(Res.string.search_leading_icon),
+                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+            )
         }
     )
 }
