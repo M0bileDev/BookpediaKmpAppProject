8837aff1cfb90129f28b671edc36783e1c7bf35c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-22 22:09:09 +0200

    add keyboard controller; create preview; set default modifier;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListScreen.kt
@@ -1,15 +1,27 @@
 package com.plcoding.bookpedia.book.presentation.booklist
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.repeatOnLifecycle
 import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.booklist.components.SearchBar
+import com.plcoding.bookpedia.core.presentation.DarkBlue
 import kotlinx.coroutines.flow.distinctUntilChanged
+import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -44,9 +56,35 @@ fun BookListScreen(
 
 @Composable
 fun BookListScreen(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     state: BookListState,
     onAction: (BookListScreenAction) -> Unit,
 ) {
+    val keyboard = LocalSoftwareKeyboardController.current
 
+    Column(
+        modifier = modifier.fillMaxWidth().background(DarkBlue).statusBarsPadding()
+    ) {
+        SearchBar(
+            modifier = Modifier.widthIn(max = 400.dp).fillMaxWidth().padding(16.dp),
+            searchQuery = state.searchQuery,
+            onSearchQueryChanged = { search ->
+                onAction(BookListScreenAction.OnSearchQueryChanged(search))
+            },
+            onImeDone = {
+                keyboard?.hide()
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewBookListScreen() {
+    MaterialTheme {
+        BookListScreen(
+            state = BookListState.createDefaultState(),
+            onAction = {}
+        )
+    }
 }
