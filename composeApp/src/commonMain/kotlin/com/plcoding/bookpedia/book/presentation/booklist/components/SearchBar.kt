5648415e90e63495226b6c1c1fde7bd00ebf7853
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-22 21:48:33 +0200

    update component with composition local;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/SearchBar.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/SearchBar.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +15,7 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
@@ -43,51 +46,58 @@ fun SearchBar(
     focusedBorderColor: Color = SandYellow,
     iconsTint: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
 ) {
-    OutlinedTextField(
-        modifier = modifier.background(backgroundColor, shape = shape)
-            .minimumInteractiveComponentSize(),
-        value = searchQuery,
-        onValueChange = onSearchQueryChanged,
-        shape = shape,
-        colors = OutlinedTextFieldDefaults.colors(
-            cursorColor = cursorColor,
-            focusedBorderColor = focusedBorderColor
-        ),
-        placeholder = {
-            Text(text = stringResource(Res.string.search_hint))
-        },
-        leadingIcon = {
-            Icon(
-                painter = painterResource(Res.drawable.search_24px),
-                contentDescription = stringResource(Res.string.search_leading_icon),
-                tint = iconsTint
-            )
-        },
-        singleLine = true,
-        keyboardActions = KeyboardActions(
-            onSearch = {
-                onImeSearch()
-            }
-        ),
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Text,
-            imeAction = ImeAction.Search
-        ),
-        trailingIcon = {
-            AnimatedVisibility(
-                visible = searchQuery.isNotBlank()
-            ) {
-                IconButton(
-                    onClick = { onSearchQueryChanged("") }
-                ) {
-                    Icon(
-                        painter = painterResource(Res.drawable.close_24px),
-                        contentDescription = stringResource(Res.string.clear_search_hint),
-                        tint = iconsTint
-                    )
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides TextSelectionColors(
+            handleColor = SandYellow,
+            backgroundColor = SandYellow
+        )
+    ) {
+        OutlinedTextField(
+            modifier = modifier.background(backgroundColor, shape = shape)
+                .minimumInteractiveComponentSize(),
+            value = searchQuery,
+            onValueChange = onSearchQueryChanged,
+            shape = shape,
+            colors = OutlinedTextFieldDefaults.colors(
+                cursorColor = cursorColor,
+                focusedBorderColor = focusedBorderColor
+            ),
+            placeholder = {
+                Text(text = stringResource(Res.string.search_hint))
+            },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(Res.drawable.search_24px),
+                    contentDescription = stringResource(Res.string.search_leading_icon),
+                    tint = iconsTint
+                )
+            },
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onImeSearch()
                 }
-            }
-        })
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            trailingIcon = {
+                AnimatedVisibility(
+                    visible = searchQuery.isNotBlank()
+                ) {
+                    IconButton(
+                        onClick = { onSearchQueryChanged("") }
+                    ) {
+                        Icon(
+                            painter = painterResource(Res.drawable.close_24px),
+                            contentDescription = stringResource(Res.string.clear_search_hint),
+                            tint = iconsTint
+                        )
+                    }
+                }
+            })
+    }
 }
 
 @Preview(name = "search_without_value")
