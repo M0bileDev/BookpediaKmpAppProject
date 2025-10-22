e9bc3aa7d93a3aa2ecf21bd0dc796b0e9661d19a
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-22 22:23:02 +0200

    update component;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookListItem.kt
@@ -2,6 +2,7 @@ package com.plcoding.bookpedia.book.presentation.booklist.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.plcoding.bookpedia.book.domain.Book
@@ -28,7 +30,12 @@ fun BookListItem(
         Row(
             modifier = Modifier.fillMaxWidth().padding(16.dp).height(IntrinsicSize.Min)
         ) {
+            Box(
+                modifier = Modifier.height(100.dp),
+                contentAlignment = Alignment.Center
+            ) {
 
+            }
         }
     }
 }
