2004168ecf8ab40695ce28c784b8617c7067e16a
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-22 22:13:22 +0200

    create book list item component;

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookListItem.kt
@@ -0,0 +1,14 @@
+package com.plcoding.bookpedia.book.presentation.booklist.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.plcoding.bookpedia.book.domain.Book
+
+@Composable
+fun BookListItem(
+    modifier: Modifier = Modifier,
+    book: Book,
+    onClick: () -> Unit
+) {
+
+}
