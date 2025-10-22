5d6def7161672a38aa775c06c906a99307440854
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-22 22:16:46 +0200

    update component;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookListItem.kt
@@ -1,8 +1,14 @@
 package com.plcoding.bookpedia.book.presentation.booklist.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.core.presentation.LightBlue
 
 @Composable
 fun BookListItem(
@@ -10,5 +16,10 @@ fun BookListItem(
     book: Book,
     onClick: () -> Unit
 ) {
+    Surface(
+        modifier = modifier.clickable(onClick = onClick).background(LightBlue.copy(alpha = 0.2f)),
+        shape = RoundedCornerShape(32.dp)
+    ) {
 
+    }
 }
