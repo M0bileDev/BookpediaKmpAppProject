6237bb49b3d7f6050bf1e78ec46c07227f77c8da
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-23 22:00:34 +0200

    update component with lazy column;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookList.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookList.kt
@@ -1,9 +1,12 @@
 package com.plcoding.bookpedia.book.presentation.booklist.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.plcoding.bookpedia.book.domain.Book
 
 @Composable
@@ -13,5 +16,9 @@ fun BookList(
     onClick: (Book) -> Unit,
     scrollState: LazyListState = rememberLazyListState()
 ) {
-
+    LazyColumn(
+        modifier = modifier,
+        state = scrollState,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) { }
 }
