3e788bd088a2b1aa4379db32a04774fa6174273b
m0bileDev <damian.ogorek.1996@gmail.com>
2025-10-23 21:58:53 +0200

    update function argument;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookList.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookList.kt
@@ -1,7 +1,7 @@
 package com.plcoding.bookpedia.book.presentation.booklist.components
 
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.plcoding.bookpedia.book.domain.Book
@@ -11,7 +11,7 @@ fun BookList(
     modifier: Modifier = Modifier,
     books: List<Book>,
     onClick: (Book) -> Unit,
-    scrollState: ScrollState = rememberScrollState()
+    scrollState: LazyListState = rememberLazyListState()
 ) {
 
 }
