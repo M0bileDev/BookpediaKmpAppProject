e746c7644a5f7e9f51d68e39beb0cf420596ce6f
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-08 12:32:49 +0100

    provide implementation of RemoteBookDataSource; provide implementation of searchBooks function;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
@@ -1,17 +1,34 @@
 package com.plcoding.bookpedia.book.data.network
 
-import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.data.dto.search.SearchedBookDto
+import com.plcoding.bookpedia.core.data.safeCall
 import com.plcoding.bookpedia.core.domain.DataError
 import com.plcoding.bookpedia.core.domain.Result
 import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+
+private const val BASE_URL = "https://openlibrary.org"
 
 class KtorRemoteBookDataSource(
     private val httpClient: HttpClient
-){
-    suspend fun searchBooks(
+) : RemoteBookDataSource {
+    override suspend fun searchBooks(
         query: String,
-        limit: Int? = null
-    ): Result<List<Book>, DataError>{
-
+        limit: Int?
+    ): Result<SearchedBookDto, DataError> {
+        return safeCall {
+            httpClient.get(
+                "$BASE_URL/search.json"
+            ) {
+                parameter("q", query)
+                parameter("limit", limit)
+                parameter("language", "eng")
+                parameter(
+                    "fields",
+                    "key,title,language,cover_i,author_key,author_name,cover_edition_key,first_publish_year,ratings_average,ratings_count,number_of_pages_median,edition_count"
+                )
+            }
+        }
     }
 }
