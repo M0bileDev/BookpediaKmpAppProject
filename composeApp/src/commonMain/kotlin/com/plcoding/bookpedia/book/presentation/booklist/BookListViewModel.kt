2ba9cab68949b8cd9b9cda66f3e1498d241ad4c3
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-08 23:04:08 +0100

    provide implementation for search and observe books search request in view model

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/BookListViewModel.kt
@@ -2,18 +2,46 @@ package com.plcoding.bookpedia.book.presentation.booklist
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.domain.BookRepository
+import com.plcoding.bookpedia.core.domain.onError
+import com.plcoding.bookpedia.core.domain.onSuccess
+import com.plcoding.bookpedia.core.presentation.toUiText
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class BookListViewModel : ViewModel() {
+class BookListViewModel(
+    private val bookRepository: BookRepository
+) : ViewModel() {
+
+    private var cachedBooks = emptyList<Book>()
+    private var searchJob: Job? = null
 
     private val _state = MutableStateFlow(BookListState.createDefaultState())
-    val state get() = _state.asStateFlow()
+    val state
+        get() = _state.onStart {
+            if (cachedBooks.isEmpty()) {
+                observeSearchQuery()
+            }
+        }.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000L),
+            _state.value
+        )
 
     private val _action = MutableSharedFlow<BookListViewModelAction>()
     val action get() = _action.asSharedFlow()
@@ -36,6 +64,57 @@ class BookListViewModel : ViewModel() {
             _state.update {
                 it.copy(selectedTabIndex = action.index)
             }
+        }
+    }
+
+    @OptIn(FlowPreview::class)
+    private fun observeSearchQuery() {
+        state.map {
+            it.searchQuery
+        }.distinctUntilChanged()
+            .debounce(500L)
+            .onEach { query ->
+                when {
+                    query.isBlank() -> {
+                        _state.update {
+                            it.copy(
+                                errorMessage = null,
+                                searchResult = cachedBooks
+                            )
+                        }
+                    }
+
+                    query.length >= 3 -> {
+                        searchJob?.cancel()
+                        searchJob = searchBooks(query)
+                    }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+    private fun searchBooks(query: String) = viewModelScope.launch {
+        _state.update { it.copy(isLoading = true) }
+        viewModelScope.launch {
+            bookRepository.searchBooks(query)
+                .onSuccess { searchResults ->
+                    cachedBooks = searchResults
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            errorMessage = null,
+                            searchResult = searchResults
+                        )
+                    }
+                }
+                .onError { error ->
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            errorMessage = error.toUiText(),
+                            searchResult = emptyList()
+                        )
+                    }
+                }
         }
     }
 }
