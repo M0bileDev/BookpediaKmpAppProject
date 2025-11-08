2c5c14035b8bb4953131c3e546d9bd5d0ddcc3a9
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-08 22:16:46 +0100

    refactor return type of interface method

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/RemoteBookDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/RemoteBookDataSource.kt
@@ -1,12 +1,12 @@
 package com.plcoding.bookpedia.book.data.network
 
-import com.plcoding.bookpedia.book.data.dto.search.SearchedBookDto
+import com.plcoding.bookpedia.book.data.dto.search.SearchedBookResponseDto
 import com.plcoding.bookpedia.core.domain.DataError
 import com.plcoding.bookpedia.core.domain.Result
 
 interface RemoteBookDataSource {
     suspend fun searchBooks(
         query: String,
-        limit: Int? = null
-    ): Result<SearchedBookDto, DataError>
+        resultLimit: Int? = null
+    ): Result<SearchedBookResponseDto, DataError.Remote>
 }
