7874c1956d4f382a9d05cae13ffec5d3df70ab42
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-09 23:14:51 +0100

    implement koin di; add view model dependency;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -2,17 +2,18 @@ package com.plcoding.bookpedia
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import com.plcoding.bookpedia.book.presentation.booklist.BookListScreen
 import com.plcoding.bookpedia.book.presentation.booklist.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+        val viewModel = koinViewModel<BookListViewModel>()
         BookListScreen(
-            viewModel = remember { BookListViewModel() },
+            viewModel = viewModel,
             navigateToBookDetails = {}
         )
     }
