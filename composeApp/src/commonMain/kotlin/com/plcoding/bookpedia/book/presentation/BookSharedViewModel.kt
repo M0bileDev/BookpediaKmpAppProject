dd22da8097a95415ed32de02dfbc843df11fc379
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-10 13:44:05 +0100

    introduce shared view model for books;

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/BookSharedViewModel.kt
@@ -0,0 +1,17 @@
+package com.plcoding.bookpedia.book.presentation
+
+import androidx.lifecycle.ViewModel
+import com.plcoding.bookpedia.book.domain.Book
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class BookSharedViewModel : ViewModel() {
+
+    private val _selectedBook = MutableStateFlow<Book?>(null)
+    val selectedBook get() = _selectedBook.asStateFlow()
+
+    fun onSelectedBook(book: Book?) {
+        _selectedBook.value = book
+    }
+
+}
