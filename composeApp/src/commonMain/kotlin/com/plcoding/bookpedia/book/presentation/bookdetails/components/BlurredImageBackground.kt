6de9f70e9770dc1ab5f294a58f89ff517c348b39
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-10 23:16:04 +0100

    update component;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/bookdetails/components/BlurredImageBackground.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/bookdetails/components/BlurredImageBackground.kt
@@ -1,13 +1,27 @@
 package com.plcoding.bookpedia.book.presentation.bookdetails.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.book_cover_hint
 import coil3.compose.rememberAsyncImagePainter
+import com.plcoding.bookpedia.core.presentation.DarkBlue
+import com.plcoding.bookpedia.core.presentation.DesertWhite
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun BlurredImageBackground(
@@ -32,5 +46,32 @@ fun BlurredImageBackground(
             }
         }
     )
+
+    Box(modifier = modifier) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .weight(0.3f)
+                    .fillMaxWidth()
+                    .background(DarkBlue),
+            ) {
+                imageLoadResult?.getOrNull()?.let { painter ->
+                    Image(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .blur(20.dp),
+                        painter = painter,
+                        contentDescription = stringResource(Res.string.book_cover_hint),
+                        contentScale = ContentScale.Crop,
+                    )
+                }
+            }
+
+            Box(modifier = Modifier
+                .weight(0.7f)
+                .fillMaxWidth()
+                .background(DesertWhite))
+        }
+    }
 
 }
