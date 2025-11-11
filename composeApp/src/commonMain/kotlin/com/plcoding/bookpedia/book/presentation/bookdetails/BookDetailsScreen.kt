41aadeb795e6f0e5f23828ae5d5baf021275759e
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-11 12:51:57 +0100

    set default value for modifier; implement blurred image background;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/bookdetails/BookDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/bookdetails/BookDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.plcoding.bookpedia.book.presentation.bookdetails
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -8,11 +9,12 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.repeatOnLifecycle
+import com.plcoding.bookpedia.book.presentation.bookdetails.components.BlurredImageBackground
 import kotlinx.coroutines.flow.distinctUntilChanged
 
 @Composable
 fun BookDetailsScreen(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     viewModel: BookDetailsViewModel,
     onBackClick: () -> Unit,
 ) {
@@ -31,8 +33,8 @@ fun BookDetailsScreen(
     }
 
     BookDetailsScreen(
-        modifier = modifier,
-        bookDetailsState = state,
+        modifier = modifier.fillMaxSize(),
+        state = state,
         onAction = { action ->
             viewModel.onAction(action)
         },
@@ -42,8 +44,16 @@ fun BookDetailsScreen(
 @Composable
 fun BookDetailsScreen(
     modifier: Modifier,
-    bookDetailsState: BookDetailsState,
+    state: BookDetailsState,
     onAction: (BookDetailsAction) -> Unit,
 ) {
+    BlurredImageBackground(
+        modifier,
+        imageUrl = state.book?.imageUrl,
+        isFavorite = state.isFavorite,
+        onFavoriteClick = { onAction(BookDetailsAction.OnFavoriteClick) },
+        onBackClick = { onAction(BookDetailsAction.OnBackClick) },
+    ) {
 
+    }
 }
