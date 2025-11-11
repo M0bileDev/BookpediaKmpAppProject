402ded6ac628660955e47c5b6a4fb32cc07ff8e3
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-11 12:31:25 +0100

    implement action listener;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/bookdetails/BookDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/bookdetails/BookDetailsScreen.kt
@@ -1,34 +1,49 @@
 package com.plcoding.bookpedia.book.presentation.bookdetails
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 @Composable
 fun BookDetailsScreen(
     modifier: Modifier,
     viewModel: BookDetailsViewModel,
-    onBackClick: () -> Unit
+    onBackClick: () -> Unit,
 ) {
 
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val lifecycle = LocalLifecycleOwner.current
 
-    BookDetailsScreen(
-        bookDetailsState = state,
-        onAction = { action ->
-            when (action) {
-                is BookDetailsAction.OnBackClick -> onBackClick()
-                else -> Unit
+    LaunchedEffect(lifecycle) {
+        lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.action.distinctUntilChanged().collect { action ->
+                when (action) {
+                    BookDetailsViewModelAction.OnNavigateBack -> onBackClick()
+                }
             }
         }
+    }
+
+    BookDetailsScreen(
+        modifier = modifier,
+        bookDetailsState = state,
+        onAction = { action ->
+            viewModel.onAction(action)
+        },
     )
 }
 
 @Composable
 fun BookDetailsScreen(
+    modifier: Modifier,
     bookDetailsState: BookDetailsState,
-    onAction: (BookDetailsAction) -> Unit
+    onAction: (BookDetailsAction) -> Unit,
 ) {
 
 }
