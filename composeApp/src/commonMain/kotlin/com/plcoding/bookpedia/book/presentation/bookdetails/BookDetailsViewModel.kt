e053b9ab7f09dd45fcb5aec151c2d2e202f4c49e
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-11 12:30:40 +0100

    implement action flow; update view model logic;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/bookdetails/BookDetailsViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/bookdetails/BookDetailsViewModel.kt
@@ -1,15 +1,33 @@
 package com.plcoding.bookpedia.book.presentation.bookdetails
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class BookDetailsViewModel : ViewModel() {
 
     private val _state = MutableStateFlow(BookDetailsState.createDefault())
     val state = _state.asStateFlow()
 
-    fun onAction(bookDetailsAction: BookDetailsAction){
-        // TODO: Not implemented yet
+    private val _action = MutableSharedFlow<BookDetailsViewModelAction>()
+    val action get() = _action.asSharedFlow()
+
+    fun onAction(action: BookDetailsAction) {
+        when (action) {
+            BookDetailsAction.OnBackClick -> {
+                viewModelScope.launch {
+                    ensureActive()
+                    _action.emit(BookDetailsViewModelAction.OnNavigateBack)
+                }
+            }
+
+            BookDetailsAction.OnFavoriteClick -> TODO()
+            is BookDetailsAction.OnSelectedBookChanged -> TODO()
+        }
     }
 }
