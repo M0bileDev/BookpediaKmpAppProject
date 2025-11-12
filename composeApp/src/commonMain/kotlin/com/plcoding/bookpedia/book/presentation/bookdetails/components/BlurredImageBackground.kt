7a83b77f9f500f11f135f3a9ef8940cceaf71b58
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-12 23:01:19 +0100

    update view by icon back arrow;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/bookdetails/components/BlurredImageBackground.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/bookdetails/components/BlurredImageBackground.kt
@@ -6,21 +6,29 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.arrow_back_24px
+import cmp_bookpedia.composeapp.generated.resources.back_arrow_hint
 import cmp_bookpedia.composeapp.generated.resources.book_cover_hint
 import coil3.compose.rememberAsyncImagePainter
 import com.plcoding.bookpedia.core.presentation.DarkBlue
 import com.plcoding.bookpedia.core.presentation.DesertWhite
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -67,10 +75,24 @@ fun BlurredImageBackground(
                 }
             }
 
-            Box(modifier = Modifier
-                .weight(0.7f)
-                .fillMaxWidth()
-                .background(DesertWhite))
+            Box(
+                modifier = Modifier
+                    .weight(0.7f)
+                    .fillMaxWidth()
+                    .background(DesertWhite)
+            )
+        }
+        IconButton(
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(16.dp)
+                .statusBarsPadding(),
+            onClick = onBackClick,
+        ) {
+            Icon(
+                painter = painterResource(Res.drawable.arrow_back_24px),
+                contentDescription = stringResource(Res.string.back_arrow_hint)
+            )
         }
     }
 
