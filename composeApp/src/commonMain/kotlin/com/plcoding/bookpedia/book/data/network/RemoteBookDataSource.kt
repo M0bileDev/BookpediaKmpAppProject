68fedcd9d6f02a813edc366ebff0c650cd2cb6d3
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-16 14:06:01 +0100

    add function that return book description over network;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/RemoteBookDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/RemoteBookDataSource.kt
@@ -1,5 +1,6 @@
 package com.plcoding.bookpedia.book.data.network
 
+import com.plcoding.bookpedia.book.data.dto.BookWorkDto
 import com.plcoding.bookpedia.book.data.dto.search.SearchedBookResponseDto
 import com.plcoding.bookpedia.core.domain.DataError
 import com.plcoding.bookpedia.core.domain.Result
@@ -9,4 +10,6 @@ interface RemoteBookDataSource {
         query: String,
         resultLimit: Int? = null
     ): Result<SearchedBookResponseDto, DataError.Remote>
+
+    suspend fun getBookDescription(bookWorkId: String): Result<BookWorkDto, DataError.Remote>
 }
