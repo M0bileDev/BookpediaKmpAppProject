fb3d8a51822404e95431ee61071ab0e7fd336375
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-16 14:05:31 +0100

    add implementation of return book description result;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/repository/DefaultBookRepository.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/repository/DefaultBookRepository.kt
@@ -11,10 +11,15 @@ import com.plcoding.bookpedia.core.domain.map
 class DefaultBookRepository(
     private val remoteBookDataSource: RemoteBookDataSource
 ) : BookRepository {
+
     override suspend fun searchBooks(
         query: String
     ): Result<List<Book>, DataError.Remote> {
         return remoteBookDataSource.searchBooks(query)
             .map { dto -> dto.results.map { searchedBookDto -> searchedBookDto.toBook() } }
+    }
+
+    override suspend fun getBookDescription(bookId: String): Result<String?, DataError> {
+        return remoteBookDataSource.getBookDescription(bookId).map { it.description }
     }
 }
