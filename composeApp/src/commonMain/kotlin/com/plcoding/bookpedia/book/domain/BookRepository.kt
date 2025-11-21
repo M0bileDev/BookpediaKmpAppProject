c8116e8db7f662df09f0f95ddc01fede818490e2
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-21 23:03:30 +0100

    update repository actions;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/BookRepository.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/BookRepository.kt
@@ -1,7 +1,9 @@
 package com.plcoding.bookpedia.book.domain
 
 import com.plcoding.bookpedia.core.domain.DataError
+import com.plcoding.bookpedia.core.domain.EmptyResult
 import com.plcoding.bookpedia.core.domain.Result
+import kotlinx.coroutines.flow.Flow
 
 interface BookRepository {
     suspend fun searchBooks(
@@ -9,4 +11,9 @@ interface BookRepository {
     ): Result<List<Book>, DataError.Remote>
 
     suspend fun getBookDescription(bookId: String): Result<String?, DataError>
+
+    fun getFavoriteBooks(): Flow<List<Book>>
+    fun isBookFavorite(id: String): Flow<Boolean>
+    suspend fun addFavoriteBook(book: Book): EmptyResult<DataError.Local>
+    suspend fun removeFavoriteBook(id: String): EmptyResult<DataError.Local>
 }
