78898c251f16c5c4059654b7dc66210b1a78332b
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-21 23:04:23 +0100

    provide implementation of dao;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/di/Modules.kt
@@ -2,6 +2,7 @@ package com.plcoding.bookpedia.di
 
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.plcoding.bookpedia.book.data.database.DatabaseFactory
+import com.plcoding.bookpedia.book.data.database.FavoriteBookDatabase
 import com.plcoding.bookpedia.book.data.network.KtorRemoteBookDataSource
 import com.plcoding.bookpedia.book.data.network.RemoteBookDataSource
 import com.plcoding.bookpedia.book.data.repository.DefaultBookRepository
@@ -27,6 +28,9 @@ val sharedModule = module {
             .create()
             .setDriver(BundledSQLiteDriver())
             .build()
+    }
+    single {
+        get<FavoriteBookDatabase>().favoriteBookDao
     }
 
     viewModelOf(::BookListViewModel)
