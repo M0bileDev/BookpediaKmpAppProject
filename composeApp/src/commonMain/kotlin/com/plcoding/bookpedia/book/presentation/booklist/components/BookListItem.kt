8a15ff1393acaf103b5e60a4bebe46953a401336
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-23 20:51:57 +0100

    update element view by adding graphic layer transitions both rotation and transition;

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/booklist/components/BookListItem.kt
@@ -1,5 +1,7 @@
 package com.plcoding.bookpedia.book.presentation.booklist.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -26,10 +28,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import cmp_bookpedia.composeapp.generated.resources.Res
 import cmp_bookpedia.composeapp.generated.resources.average_rating_x
 import cmp_bookpedia.composeapp.generated.resources.book_cover_hint
@@ -38,6 +42,7 @@ import cmp_bookpedia.composeapp.generated.resources.chevron_right_24px
 import cmp_bookpedia.composeapp.generated.resources.details_icon_hint
 import cmp_bookpedia.composeapp.generated.resources.star_24px
 import cmp_bookpedia.composeapp.generated.resources.star_hint
+import coil3.compose.AsyncImagePainter
 import coil3.compose.rememberAsyncImagePainter
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.presentation.LightBlue
@@ -85,6 +90,16 @@ fun BookListItem(
                     }
                 )
 
+                val painterState by painter.state.collectAsStateWithLifecycle()
+                val transition by animateFloatAsState(
+                    targetValue = if(painterState is AsyncImagePainter.State.Success){
+                        1f
+                    }else{
+                        0f
+                    },
+                    animationSpec = tween(durationMillis = 800)
+                )
+
                 when (val result = imageResult) {
                     null -> PulseAnimations(
                         modifier = Modifier.size(60.dp)
@@ -95,7 +110,12 @@ fun BookListItem(
                             modifier = Modifier.aspectRatio(
                                 0.65f,
                                 matchHeightConstraintsFirst = true
-                            ),
+                            ).graphicsLayer{
+                                rotationX = (1f - transition) * 30f
+                                val scale = 0.8f + (0.2f * transition)
+                                scaleX = scale
+                                scaleX = scale
+                            },
                             painter = if (result.isSuccess) painter else painterResource(Res.drawable.book_error_2),
                             contentDescription = stringResource(Res.string.book_cover_hint),
                             contentScale = if (result.isSuccess) ContentScale.Crop else ContentScale.Fit,
